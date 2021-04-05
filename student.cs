bd6170ef942ad63c5119a2503bd254c140cda5f3
thinhanh13 <71740732+thinhanh13@users.noreply.github.com>
2021-04-05 16:46:09 +0700

    Update student.cs

--- student.cs
+++ student.cs
@@ -6,9 +6,9 @@ namespace WpfApp1
 {
     class Student
     {
-        // 1.Định nghĩa trường Name
+         1.Định nghĩa trường Name
         private string Name;
-        // 2.Định nghĩa thuộc tính StudentName từ trường Name
+        2.Định nghĩa thuộc tính StudentName từ trường Name
         public string StudentName
         {
             get
@@ -20,18 +20,5 @@ namespace WpfApp1
                 Name = value;
             }
         }
-        //3.Định nghĩa phương thức khởi tạo Student(string stName)
-        public void Student(string stName)
-        
-            Console.WriteLine("Student.");
-        }
-   
-    private void btnTalk_Click(object sender, RoutedEventArgs e)
-    {
-        Student st1 = new Student("Minh");
-        st.Talk();
-        Student st2 = new Student();
-        st1.StudentName = "Dung";
-        st1.Talk();
-    }
-}
+        3.Định nghĩa phương thức khởi tạo Student(string stName)
+       
